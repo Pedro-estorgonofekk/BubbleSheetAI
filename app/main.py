2c517fe7dfc58b8a9d9127567aeb2be51766b0b7
unknown <pedroalvarenga173@gmail.com>
2026-03-10 22:29:10 -0300

    feat: rotas de geração e correção

--- app/main.py
+++ app/main.py
@@ -7,4 +7,14 @@ def mensagem():
     print("relou uordi")
     return("relou uordi")
 
+@app.route('/gerar-gabarito', methods=['POST'])
+def mensagem():
+    print("Geração de gabarito")
+    return("Geração de gabarito")
+
+@app.route('/corrigir-gabarito', methods=['POST'])
+def mensagem():
+    print("Correção de gabarito")
+    return("Correção de gabarito")
+
 app.run(debug=True, port=3000)
